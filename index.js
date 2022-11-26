2b60ddbe2d3b710316eb959f0fed27b1789eacea
Abhishek Singh <50039604+Abhishek2122@users.noreply.github.com>
2022-11-26 00:43:57 -0800

    add.new code..

--- index.js
+++ index.js
@@ -38,19 +38,6 @@ fs.createReadStream('customer.csv')
         })
     }
 });
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-   res.send('Hello World');
-});
-
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
-
 function createTable(con,tableName,columnName){
     return new Promise((resolve,reject)=>{
         var column='(';
